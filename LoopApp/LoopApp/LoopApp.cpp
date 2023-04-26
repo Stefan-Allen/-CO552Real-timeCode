0c6d8a2ee3daa7ecc7511c1e9d43ee8a7cb85e3b
stefan <stefanallen10@gmail.com>
2023-04-26 03:28:48 +0100

    loop c++

---
+++ LoopApp/LoopApp/LoopApp.cpp
@@ -0,0 +1,47 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    srand(time(0));
+    for(;;)
+    {
+        int i = rand() % 10;
+        switch (i)
+        {
+        case 1:
+            std::cout << "Sensor ID: 1 Triggered" << std::endl;
+            break;
+        case 2:
+            std::cout << "Alarm ID: 1 Triggered" << std::endl;
+            break;
+        case 3:
+            std::cout << "Alarm ID: 2 Triggered" << std::endl;
+            break;
+        case 4:
+            std::cout << "Sensor ID: 2 Triggered" << std::endl;
+            break;
+        case 5:
+            std::cout << "Alarm ID: 3 Triggered" << std::endl;
+            break;
+        case 6:
+            std::cout << "Sensor ID: 3 Triggered" << std::endl;
+            break;
+        case 7:
+            std::cout << "Alarm ID: 4 Triggered" << std::endl;
+            break;
+        case 8:
+            std::cout << "Sensor ID: 4 Triggered" << std::endl;
+            break;
+        case 9:
+            std::cout << "Alarm ID: 5 Triggered" << std::endl;
+            break;
+        case 10:
+            std::cout << "Sensor ID: 5 Triggered" << std::endl;
+            break;
+        default:
+            std::cout << "Manual Alarm Triggered" << std::endl;
+        }
+    }
+}
